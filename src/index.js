80ce5d2f7e6456ec54b56be718ab055166c1fa50
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 03:03:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import './styles/App.css';
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// Create a root and render App inside <div id="root">
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
